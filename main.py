ebae97bae22330046dec9ddeb375840c3df74f44
Chen Zhang <aminodevis263@gmail.com>
2026-03-13 20:06:36 +0800

    feat: add argparse to main.py

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import argparse
+
+
+ascii_title = r"""
+   ____          _      _____  _____  ______      _____        _____   _____ ______ _____  
+  / __ \   /\   | |    |  __ \|  __ \|  ____|    |  __ \ /\   |  __ \ / ____|  ____|  __ \ 
+ | |  | | /  \  | |    | |  | | |__) | |__ ______| |__) /  \  | |__) | (___ | |__  | |__) |
+ | |  | |/ /\ \ | |    | |  | |  ___/|  __|______|  ___/ /\ \ |  _  / \___ \|  __| |  _  / 
+ | |__| / ____ \| |____| |__| | |    | |____     | |  / ____ \| | \ \ ____) | |____| | \ \ 
+  \____/_/    \_\______|_____/|_|    |______|    |_| /_/    \_\_|  \_\_____/|______|_|  \_\                                                                      
+        
+    OALDPE-PARSER
+ ------------------------------------------------------------------------------------------
+"""
+
+
 def main():
-    print("Hello from oaldpe-dict-parser!")
+    parser = argparse.ArgumentParser(
+        description=ascii_title, formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument("-w", "--word", type=str, help="look up a single word")
+    parser.add_argument(
+        "-e", "--excel", type=str, help="look up the words contained in an excel sheet"
+    )
+
+    args = parser.parse_args()
+    if args.word:
+        pass
+    elif args.excel:
+        pass
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
